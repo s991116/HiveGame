2d3a0bbeb033311d46f9af69a7242f0ef72fc471
s991116 <philip.juhl@gmail.com>
2024-05-10 14:40:58 +0200

    Fixed Cyclic import error, with replacing from x import x with import x as x

--- app/Piece.py
+++ app/Piece.py
@@ -13,8 +13,3 @@ class Piece:
       return self.creature.value.upper() + str(self.index)
     else:
       return self.creature.value.lower() + str(self.index)
-
-  def __eq__(self, other: any): # type: ignore
-    if isinstance(other, Piece):
-      return self.firstPlayer == other.firstPlayer and self.creature == other.creature and self.index == other.index # type: ignore
-    return False
