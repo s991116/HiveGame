b9b7c6f19d6bd66ddbb7a89c9c29989c1e1701ec
s991116 <philip.juhl@gmail.com>
2024-05-11 11:21:57 +0200

    Added movement for grasshopper and test passes

--- app/PieceRulesGrasshopper.py
+++ app/PieceRulesGrasshopper.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import List
 from app.PieceRules import PieceRules
 from app.Coordinate import Coordinate
+from app.Directions import Direction
 import app.BoardPiece as BP
 import app.HiveBoard as HB
 import app.HivePieces as HP
@@ -9,4 +10,19 @@ import app.HivePieces as HP
 class PieceRulesGrasshopper(PieceRules):
 
   def getMoves(self, boardPiece: BP.BoardPiece, board: HB.HiveBoard) -> List[BP.BoardPiece]:
-    return [HP.HivePieces.CreateCloneWithCoordinate(boardPiece, Coordinate(0,0))]
+
+    directions = [Direction.LEFT, Direction.UP_LEFT, Direction.UP_RIGHT, Direction.RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_LEFT]
+    moves: List[BP.BoardPiece] = []
+    for direction in directions:
+      directionCoordinate = boardPiece.coordinate
+      piecePresent = True
+      distance = 0
+      while piecePresent:
+        directionCoordinate = board.navigate(direction, directionCoordinate)
+        piecePresent = not board.isPlaceFree(directionCoordinate)
+        distance += 1
+      if distance > 1:
+        move = HP.HivePieces.CreateCloneWithCoordinate(boardPiece, directionCoordinate)
+        moves.append(move)
+
+    return moves
