df332f15d1abed4db6ecf5e371284d8dfe093da5
s991116 <philip.juhl@gmail.com>
2024-05-11 08:27:00 +0200

    Improved Grasshopper movement test, to test movement in 2 dirrections

--- tests/test_movement_species.py
+++ tests/test_movement_species.py
@@ -3,6 +3,7 @@ from app.HiveGame import HiveGame
 from app.Coordinate import Coordinate
 from app.Creatures import Creatures
 from app.HivePieces import HivePieces
+from app.Piece import Piece
 
 param_list = [('a', 'a'), ('a', 'b'), ('b', 'b')]
 
@@ -12,19 +13,30 @@ class TestMovmentSpecies(unittest.TestCase):
         #Arrange
         hiveGame = HiveGame()
         pieces = hiveGame.board.pieces
+
         hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.QueenBee_P1, Coordinate(0,0)))
         hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Ant_0_P2, Coordinate(-1,0)))
         hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Grasshopper_0_P1, Coordinate(1,0)))
-        hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Ant_1_P2, Coordinate(-2,0)))
+        hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Ant_0_P2, Coordinate(-2,0)))
+        hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Ant_1_P1, Coordinate(0,1)))
+        hiveGame.playMove(HivePieces.CreateCloneWithCoordinate(pieces.Ant_1_P2, Coordinate(-3,0)))
 
-        print(hiveGame.board.printBoard)
+        print(hiveGame.board.printBoard())
 
         #Act
-        movementMoves = hiveGame.rules.getMovementMoves()
+        g = hiveGame.board.findPiece(pieces.Grasshopper_0_P1.piece)
+        if g is not None:
+            grassHopperMoves = g.getMoves(hiveGame.board)
+        else:
+            grassHopperMoves = []
 
         #Assert
-        self.assertEqual(len(movementMoves), 1)
-        self.assertEqual(movementMoves[0].piece.creature, Creatures.Grasshopper)
+        self.assertEqual(len(grassHopperMoves), 2)
+        move1 = HivePieces.CreateCloneWithCoordinate(pieces.Grasshopper_0_P1, Coordinate(-4,0))
+        self.assertIn(move1, grassHopperMoves)
+        move2 = HivePieces.CreateCloneWithCoordinate(pieces.Grasshopper_0_P1, Coordinate(0,2))
+        self.assertIn(move2, grassHopperMoves)
+
 
     def test_movement_after_P1_Queen_is_placed(self):
         #Arrange
