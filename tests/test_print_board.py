9431eff68ee3b9e041a23042c47b53c23b2d8eb6
s991116 <philip.juhl@gmail.com>
2024-05-14 06:53:20 +0200

    Added test that prints board with empty space between pieces on same line

--- tests/test_print_board.py
+++ tests/test_print_board.py
@@ -56,6 +56,32 @@ class TestPrintBoard(unittest.TestCase):
         number_of_lines = boardPrint.count('\n')
         self.assertEqual(1+9, number_of_lines)
 
+    def test_printBoard_empty_space_between_lines(self):
+        #Arrange
+        hiveBoard = HiveBoard()
+        hivePieces = HivePieces()
+
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Beetle_0_P1, Coordinate(0,0)))
+
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Ant_0_P1, Coordinate(0,1)))
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Ant_1_P1, Coordinate(1,1)))
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Ant_2_P1, Coordinate(2,1)))
+
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Beetle_1_P1, Coordinate(3,0)))
+
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Grasshopper_0_P1, Coordinate(2,-1)))
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Grasshopper_1_P1, Coordinate(1,-1)))
+        hiveBoard.movePiece(HivePieces.CreateCloneWithCoordinate(hivePieces.Grasshopper_2_P1, Coordinate(0,-1)))
+
+        #Act
+        boardPrint = hiveBoard.printBoard()
+        print(boardPrint)
+
+        #Assert
+        number_of_lines = boardPrint.count('\n')
+        self.assertEqual(1+9, number_of_lines)
+
+
 
 if __name__ == "__main__":
     unittest.main()
